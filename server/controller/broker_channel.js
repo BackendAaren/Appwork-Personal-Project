3b04847a32c9fc31967fe65c8cd94ec3fa8b21f0
Aaren <aaren.backend.ow95@gmail.com>
2024-06-15 20:34:22 +0800

    Feat: Add Auto acknowledge system for message queue broker

--- server/controller/broker_channel.js
+++ server/controller/broker_channel.js
@@ -1,8 +1,17 @@
 export class MessageType {
   constructor(channel, messageType, payload) {
+    this.messageId = messageId; //information unique id
     this.channel = channel;
     this.messageType = messageType;
     this.payload = payload;
+  }
+}
+
+class Message {
+  constructor(id, content) {
+    this.id = id;
+    this.content = content;
+    this.acknowledged = false;
   }
 }
 
@@ -12,16 +21,16 @@ export class MessageQueue {
     this.waiting = {};
   }
 
-  async enqueue(channel, message) {
+  async enqueue(channel, messageType, payload) {
+    const message = new MessageType(channel, messageType, payload);
     if (!this.channels[channel]) {
       this.channels[channel] = [];
     }
-    this.channels[channel].push(message);
-    //若dequeue進入等待狀態，enqueue將information推入channel將resolve取出告知dequeue繼續運作
-    if (this.waiting[channel] && this.waiting[channel].length > 0) {
-      const resolveNext = this.waiting[channel].shift();
-      resolveNext();
-    }
+
+    const messageId = this.channels[channel].length + 1;
+    const newMessage = new Message(messageId, message);
+    this.channels[channel].push(newMessage);
+    this.processNext(channel);
   }
 
   async dequeue(channel) {
@@ -29,13 +38,40 @@ export class MessageQueue {
       this.channels[channel] = [];
     }
 
-    while (this.channels[channel].length === 0) {
-      if (!this.waiting[channel]) {
-        this.waiting[channel] = [];
+    while (true) {
+      if (this.channels[channel].length > 0) {
+        const message = this.channels[channel][0];
+        if (message.acknowledged) {
+          this.channels[channel].shift();
+          return message.content;
+        }
       }
-      //當channel裡面沒有information時將resolve推入waiting進入等待狀態
-      await new Promise((resolve) => this.waiting[channel].push(resolve));
+      await new Promise((resolve) => {
+        if (!this.waiting[channel]) {
+          this.waiting[channel] = [];
+        }
+        this.waiting[channel].push(resolve);
+      });
     }
-    return this.channels[channel].shift();
+  }
+
+  acknowledge(channel, messageId) {
+    const channelMessage = this.channels[channel];
+    if (!channelMessage || channelMessage.length === 0) {
+      return;
+    }
+    const message = channelMessage.find((msg) => msg.id === messageId);
+    if (message) {
+      message.acknowledged = true;
+    }
+
+    this.processNext(channel);
+  }
+
+  processNext(channel) {
+    if (this.waiting[channel] && this.waiting[channel].length > 0) {
+      const resolveNext = this.waiting[channel].shift();
+      resolveNext();
+    }
   }
 }
--- test.js
+++ test.js
@@ -1,10 +1,85 @@
-// get the reference of EventEmitter class of events module
-import events from "events";
-//create an object of EventEmitter class by using above reference.
-var em = new events.EventEmitter();
-//綁定事件函式
-em.on("FirstEvent", function (data) {
-  console.log("First subscriber:" + data);
-});
-//emit觸發事件
-em.emit("FirstEvent", "我已經進入了第一事件的監聽函式!");
+// MessageType class
+export class MessageType {
+  constructor(messageId, channel, messageType, payload) {
+    this.messageId = messageId; // 每個訊息的唯一識別碼
+    this.channel = channel;
+    this.messageType = messageType;
+    this.payload = payload;
+  }
+}
+
+// MessageQueue class
+export class MessageQueue {
+  constructor() {
+    this.channels = {};
+    this.waiting = {};
+    this.pendingMessages = {}; // 追踪等待確認回執的訊息
+  }
+
+  async enqueue(channel, message) {
+    if (!this.channels[channel]) {
+      this.channels[channel] = [];
+    }
+
+    // Assign a unique messageId to the message
+    const messageId = Math.random().toString(36).substr(2, 9); // 簡易的唯一識別碼產生方式
+
+    const messageTypeObj = new MessageType(
+      messageId,
+      channel,
+      message.messageType,
+      message.payload
+    );
+
+    this.channels[channel].push(messageTypeObj);
+
+    // 將訊息加入待確認回執的列表
+    this.pendingMessages[messageId] = messageTypeObj;
+
+    // 若 dequeue 進入等待狀態，enqueue 將 resolve 通知 dequeue 繼續運作
+    if (this.waiting[channel] && this.waiting[channel].length > 0) {
+      const resolveNext = this.waiting[channel].shift();
+      resolveNext();
+    }
+
+    // 返回 messageId，供接收者後續發送確認回執使用
+    return messageId;
+  }
+
+  async dequeue(channel) {
+    if (!this.channels[channel]) {
+      this.channels[channel] = [];
+    }
+
+    while (this.channels[channel].length === 0) {
+      if (!this.waiting[channel]) {
+        this.waiting[channel] = [];
+      }
+      await new Promise((resolve) => this.waiting[channel].push(resolve));
+    }
+
+    // 取出訊息
+    const messageTypeObj = this.channels[channel].shift();
+    const messageId = messageTypeObj.messageId;
+
+    // 等待確認回執
+    while (!this.pendingMessages[messageId]) {
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // 等待一段時間後重新檢查
+    }
+
+    // 從待確認回執的列表中刪除該訊息
+    delete this.pendingMessages[messageId];
+
+    return messageTypeObj;
+  }
+
+  // 接收者使用此方法來發送確認回執
+  async acknowledge(messageId) {
+    if (this.pendingMessages[messageId]) {
+      delete this.pendingMessages[messageId]; // 刪除待確認回執的列表中的該訊息
+      return true; // 確認成功
+    } else {
+      return false; // 訊息不存在或已經確認過
+    }
+  }
+}
